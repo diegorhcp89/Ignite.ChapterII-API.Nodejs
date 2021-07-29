350c25f0da6a1401d4f67fa83e7d2e722247bcb5
Diego <diegoferreiradoamaral@gmail.com>
2021-07-28 21:43:27 -0300

    Criando controller de usuário

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,9 +1,9 @@
-import { inject } from "tsyringe"
+import { inject, injectable } from "tsyringe"
 
 import { IUsersRepository } from "../../repositories/IUsersRepository"
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO"
 
-
+@injectable()
 class CreateUserUseCase {
 
     constructor(
