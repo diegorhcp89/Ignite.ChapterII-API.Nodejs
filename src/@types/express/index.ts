b35ff1e3da9888b2708d9228b689e6947486da50
Diego <diegoferreiradoamaral@gmail.com>
2022-01-20 23:06:13 -0300

    Adicionando coluna de avatar

---
+++ src/@types/express/index.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Express {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  export interface Request {
+    user: {
+      id: string;
+    };
+  }
+
+  // eslint-disable-next-line prettier/prettier
+}
